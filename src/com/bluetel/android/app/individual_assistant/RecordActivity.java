2c9ce75906f1ec3c600c91e9a68143a18940090a
zhangss007 <1254295992@qq.com>
2014-03-06 16:17:34 +0800

    add some resource file

--- src/com/bluetel/android/app/individual_assistant/RecordActivity.java
+++ src/com/bluetel/android/app/individual_assistant/RecordActivity.java
@@ -2,15 +2,24 @@ package com.bluetel.android.app.individual_assistant;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class RecordActivity extends Activity{
 
+	private ImageView recordImage ;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.record_layout) ;
+		findViews() ;
 	}
 
+	private void findViews(){
+	
+		recordImage = (ImageView)findViewById(R.id.record_image) ;
+		
+	}
 	
 }
